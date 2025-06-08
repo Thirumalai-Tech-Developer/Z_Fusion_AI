d0feb7d926493b50ad101665aab52d26b21c3ad6
Thirumalai-Tech-Developer <thirutechdeveloper@gmail.com>
2025-06-08 19:05:24 +0530

    chore: update Vite configuration and add Vercel rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     runtimeErrorOverlay()
@@ -15,7 +17,6 @@ export default defineConfig({
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  // root: path.resolve(__dirname, "src"),
   build: {
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
